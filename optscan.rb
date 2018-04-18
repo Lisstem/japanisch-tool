4ce6d4155ca4b860964592d76a76da6b91e3b9ab
lisstem <thole.goesmann@gmx.de>
2018-04-18 19:30:03 +0200

    optscan now is a module optscan now can include multicharflags with --

--- optscan.rb
+++ optscan.rb
@@ -1,35 +1,40 @@
-def optscan(string, downcase = true)
-  unless string.respond_to? :each
-    string = string.split(' ')
-  end
-  args = {:flags => [], :params => [], :options => {}}
-  flag = nil
-  string.each do |s|
-    if s[0] == '-'
-      unless flag.nil?
-        raise ArgumentError, "No value given for option #{flag}."
-      end
-      if s[1] == '-'
-        s.downcase! if downcase
-        flag = s[2..-1]
-        flag = nil if flag == ''
+module OptScan
+  def self.optscan(string, downcase = true)
+    unless string.respond_to? :each
+      string = string.split(' ')
+    end
+    args = {:flags => [], :params => [], :options => {}}
+    flag = nil
+    string.each do |s|
+      if s[0] == '-'
+        unless flag.nil?
+          args[:flags] << flag
+        end
+        if s[1] == '-'
+          s.downcase! if downcase
+          flag = s[2..-1]
+          flag = nil if flag == ''
+        else
+          s[1..-1].each_char do |char|
+            args[:flags] << char
+          end
+          flag = nil
+        end
       else
-        s[1..-1].each_char do |char|
-          args[:flags] << char
+        if flag.nil?
+          args[:params] << s
+        else
+          args[:options][flag] = [] if args[:options][flag].nil?
+          args[:options][flag] << s
+          flag = nil
         end
       end
-    else
-      if flag.nil?
-        args[:params] << s
-      else
-        args[:options][flag] = [] if args[:options][flag].nil?
-        args[:options][flag] << s
-        flag = nil
-      end
     end
+    args[:flags] << flag unless flag.nil?
+    args
   end
-  args
 end
+
 
 =begin
 # -Asda sDsdd --As akdsj ads --d ajsd --as aj adg asd
@@ -38,6 +43,6 @@ input = 'asd'
 exits = ['q', 'quit', 'exit']
 while !input.nil? && !exits.include?(input.strip.downcase)
   input = gets
-  puts optscan(input) unless input.nil?
+  puts OptScan::optscan(input) unless input.nil?
 end
 =end
